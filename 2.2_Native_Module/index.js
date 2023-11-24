ef24184c1c8b129a08533fba88e3c712725d634c
Shubham <razzput19102000@gmail.com>
2023-11-24 18:05:43 +0530

    Native_Module

--- 2.2_Native_Module/index.js
+++ 2.2_Native_Module/index.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
-// fs.writeFile('message.txt', "Hello from Node.js", (err) => {
-//     if (err) throw err;
-//     console.log('The file has been saved');
-// });
+// fs.writeFile('message.txt','node file created',(err)=>{
+//     if(err) throw err;
+//     console.log(`The file is a saved`);
+// })
 
 fs.readFile('./message.txt',"utf-8",(err, data) => {
     if(err) throw err;
