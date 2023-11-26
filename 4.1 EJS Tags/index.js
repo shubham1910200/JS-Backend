55c2bea5b2c2eeaaffeaf20b126d3558d88b46f1
Shubham <razzput19102000@gmail.com>
2023-11-26 11:56:50 +0530

    '4.1 EJS Tags

--- 4.1 EJS Tags/index.js
+++ 4.1 EJS Tags/index.js
@@ -1,17 +1,34 @@
-import express from "express";
+// import express from "express";
+// const app = express();
+// const port = 3000;
+
+// app.get("/", (req, res) => {
+//   const data = {
+//     title: "EJS Tags",
+//     seconds: new Date().getSeconds(),
+//     items: ["apple", "banana", "cherry"],
+//     htmlContent: "<strong>This is some strong text</strong>",
+//   };
+//   res.render("index.ejs", data);
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
+import express from 'express';
 const app = express();
 const port = 3000;
+app.get('/', (req, res) => {
+  let data = {
+    title:'EJS Tags',
+    day: new Date().getDay(),
+    days:["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
 
-app.get("/", (req, res) => {
-  const data = {
-    title: "EJS Tags",
-    seconds: new Date().getSeconds(),
-    items: ["apple", "banana", "cherry"],
-    htmlContent: "<strong>This is some strong text</strong>",
   };
-  res.render("index.ejs", data);
-});
+  res.render('index.ejs', data);
+})
 
-app.listen(port, () => {
+app.listen(port,()=>{
   console.log(`Server is running on port ${port}`);
-});
+})
