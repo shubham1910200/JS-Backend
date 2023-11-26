6f8b780096be5b7221f8d2518f17ec5206cd62e9
Shubham <razzput19102000@gmail.com>
2023-11-26 11:10:28 +0530

    '4.0 EJS

--- 4.0 EJS/index.js
+++ 4.0 EJS/index.js
@@ -1,27 +1,47 @@
+// import express from 'express';
+
+// const app = express();
+// const port = 3000;
+
+// app.get('/', (req, res) => {
+//     const today = new Date("June 24 2023");
+//     const day = today.getDay();
+
+//     let type = "a weekday";
+//     let adv = "it's time to work hard";
+
+//     if(day===0 || day===6)
+//     {
+//         type = "the weekand";
+//         adv = "it's time to have some fun";
+
+//     }
+
+
+
+//     res.render('index.ejs',{dayType:type,advice:adv})
+// })
+
+// app.listen(port,()=>{
+//     console.log(`listening on ${port}`);
+// })
+
 import express from 'express';
 
 const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => {
-    const today = new Date("June 24 2023");
-    const day = today.getDay();
-
-    let type = "a weekday";
-    let adv = "it's time to work hard";
-
-    if(day===0 || day===6)
+const port  = 3000;
+let day = 'Working Day'
+let throught = 'Work in Progress'
+app.get('/', (req, res)=>{
+    let date = new Date();
+     day = date.getDay();
+    if(day===0)
     {
-        type = "the weekand";
-        adv = "it's time to have some fun";
-
+        day = 'Weekend';
+        throught = 'Sleeping😊'
     }
-
-
-
-    res.render('index.ejs',{dayType:type,advice:adv})
+    res.render('index.ejs',{dayType:day,advice:throught})
 })
-
 app.listen(port,()=>{
     console.log(`listening on ${port}`);
 })
