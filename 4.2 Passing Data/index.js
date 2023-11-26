f84b27411f0577d824a66645918f96d92cacaa1b
Shubham <razzput19102000@gmail.com>
2023-11-26 12:14:15 +0530

    4.2 Passing Data

--- 4.2 Passing Data/index.js
+++ 4.2 Passing Data/index.js
@@ -1,21 +1,26 @@
-import express from "express";
-import bodyParser from "body-parser";
+
+
+import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended:true}))
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res.render('index.ejs')
-});
-
-app.post("/submit", (req, res) => {
-  const numberOfLetters=req.body['fName'].length + req.body['lName'].length;
+})
+app.post('/submit',(req, res) => {
+  const numberOfLetters = req.body['fName'].length + req.body['lName'].length;
   console.log(numberOfLetters);
-  res.render('index.ejs',{numberOfLetters:numberOfLetters});
-});
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+  res.render('index.ejs',
+  {
+    numberOfLetters:numberOfLetters,
+    fName:req.body['fName'],
+    lName:req.body['lName']
+  })
+})
+app.listen(port,()=>{
+  console.log(`Starting server on port ${port}`);
+})
